b8d6df6b0e4504da8160f86f72ffc11680ea56e4
fishy <173887825+FishlandicFishy@users.noreply.github.com>
2024-11-19 22:47:59 +0800

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,20 @@
+# CONFIG
+gaslitno = 11191006 # preset = 11191006
+
+# Stuff
+h1 = input()
+h = h1
+print(h)
+h2 = input()
+h = h + " * " + h2
+print(h)
+h3 = input()
+h = h + " * " + h3
+print(h)
+r = int(h1)*int(h2)*int(h3)
+print(h + " = " + str(r))
+mischief = gaslitno - r
+print(r)
+i1 = input()
+print(str(r) + " + " + str(mischief))
+print(str(r) + " + " + str(mischief) + " = " + str(gaslitno))
